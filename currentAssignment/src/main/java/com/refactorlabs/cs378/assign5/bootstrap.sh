e8efe485ddbc9a4d034a2e872e98bc0368d6aa06
Louis Pujol <lap2456@cs.utexas.edu>
2015-02-23 13:31:08 -0600

    Updated bootstrap file

--- currentAssignment/src/main/java/com/refactorlabs/cs378/assign5/bootstrap.sh
+++ currentAssignment/src/main/java/com/refactorlabs/cs378/assign5/bootstrap.sh
@@ -2,5 +2,4 @@
 
 echo "export HADOOP_USER_CLASSPATH_FIRST=true" > /home/hadoop/conf/hadoop-user-env.sh
 
-## Replace s3n://utcs378/jars/bdp-0.5.jar with the path to your JAR file.
-echo "export HADOOP_CLASSPATH=\"s3n://utcs378/jars/bdp-0.5.jar\"" >> /home/hadoop/conf/hadoop-user-env.sh
+echo "export HADOOP_CLASSPATH=\"s3n://utcs378/lap2456/jars/bdp-0.5.jar\"" >> /home/hadoop/conf/hadoop-user-env.sh
