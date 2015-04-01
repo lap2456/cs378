85f4843bef6b0a25f03b30291f4525cb9711f387
Louis Pujol <louispujol@utexas.edu>
2015-04-01 02:06:07 -0500

    deleting prev files to save space and avoid conflicts

--- currentAssignment/src/main/java/com/refactorlabs/cs378/assign5/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "export HADOOP_USER_CLASSPATH_FIRST=true" > /home/hadoop/conf/hadoop-user-env.sh
-
-echo "export HADOOP_CLASSPATH=\"s3n://utcs378/lap2456/jars/bdp-0.5.jar\"" >> /home/hadoop/conf/hadoop-user-env.sh
